3749f3ab36c43101b3478f5bebb9480ad67deead
ana <ss@g>
2026-05-13 11:24:28 -0500

    implement adjustments for catch rate adjust mods

--- osu.Game.Rulesets.Catch/Mods/CatchModDaycore.cs
+++ osu.Game.Rulesets.Catch/Mods/CatchModDaycore.cs
@@ -1,11 +1,25 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Catch.Mods
 {
     public class CatchModDaycore : ModDaycore
     {
+        public override BindableBool AdjustWindows { get; } = new BindableBool();
+
+        public override void ApplyToDifficulty(BeatmapDifficulty difficulty)
+        {
+            if (AdjustApproachRate.Value)
+            {
+                double preempt = IBeatmapDifficultyInfo.DifficultyRange(difficulty.ApproachRate, CatchHitObject.PREEMPT_RANGE);
+                preempt *= SpeedChange.Value;
+                difficulty.ApproachRate = (float)IBeatmapDifficultyInfo.InverseDifficultyRange(preempt, CatchHitObject.PREEMPT_RANGE);
+            }
+        }
     }
 }
