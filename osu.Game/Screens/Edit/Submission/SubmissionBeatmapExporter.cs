4a54f4929dda77ccbcf56b58aa307d4cea5d7351
ShikkesoraSIM <148418529+ShikkesoraSIM@users.noreply.github.com>
2026-08-24 00:59:40 +0800

    submit: una diff que ya se subio en otro lado entra como diff nueva
    si metes en un set una diff que ya subiste suelta, se lleva su beatmap id puesto y la subida entera moria con 'has BeatmapID X that has not been assigned to it by the server', que no le dice nada a nadie. mudarle el id no es una opcion (se lo estariamos robando al mapa original junto con sus scores), asi que entra como diff nueva y queda anotado en el log. de paso, los ids nuevos salen de los que no reclama NINGUNA diff del set: antes se agarraba el primero que sobraba y podia quedarse con el de una diff que todavia no habiamos exportado.

--- osu.Game/Screens/Edit/Submission/SubmissionBeatmapExporter.cs
+++ osu.Game/Screens/Edit/Submission/SubmissionBeatmapExporter.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Logging;
 using osu.Framework.Platform;
 using osu.Game.Beatmaps;
 using osu.Game.Database;
@@ -15,6 +16,13 @@ namespace osu.Game.Screens.Edit.Submission
     {
         private readonly uint? beatmapSetId;
         private readonly HashSet<int>? allocatedBeatmapIds;
+
+        /// <summary>
+        /// IDs the server allocated that no difficulty in this set claims: the pool for
+        /// difficulties that need a new one. Worked out on the first difficulty exported,
+        /// because until then we don't know which IDs are spoken for.
+        /// </summary>
+        private HashSet<int>? freeBeatmapIds;
 
         public SubmissionBeatmapExporter(Storage storage, PutBeatmapSetResponse putBeatmapSetResponse)
             : base(storage)
@@ -38,13 +46,26 @@ namespace osu.Game.Screens.Edit.Submission
                     return;
                 }
 
-                if (playableBeatmap.BeatmapInfo.OnlineID > 0)
-                    throw new InvalidOperationException($@"Difficulty ""{playableBeatmap.BeatmapInfo.DifficultyName}"" has BeatmapID {playableBeatmap.BeatmapInfo.OnlineID} that has not been assigned to it by the server!");
+                // torii: los ids que quedan libres son los que no reclama NINGUNA diff del
+                // set, no "el primero que sobre": si no, una diff nueva se queda con el id
+                // de una que todavia no exportamos y esa despues no encuentra el suyo.
+                freeBeatmapIds ??= allocatedBeatmapIds
+                                   .Except(beatmapSet.Beatmaps.Select(b => b.OnlineID))
+                                   .ToHashSet();
 
-                if (allocatedBeatmapIds.Count == 0)
+                if (freeBeatmapIds.Count == 0)
                     throw new InvalidOperationException(@"Ran out of new beatmap IDs to assign to unsubmitted beatmaps!");
 
-                int newId = allocatedBeatmapIds.First();
+                // torii: una diff con un id que este set no tiene es una que ya se subio en
+                // otro lado y ahora la estan metiendo aca. no se le puede mudar el id (se lo
+                // estariamos robando al mapa original, con sus scores), asi que entra como
+                // diff nueva. antes esto reventaba la subida entera con un error que no le
+                // decia nada a nadie.
+                if (playableBeatmap.BeatmapInfo.OnlineID > 0)
+                    Logger.Log($@"Difficulty ""{playableBeatmap.BeatmapInfo.DifficultyName}"" carried BeatmapID {playableBeatmap.BeatmapInfo.OnlineID} from another set; it will be submitted as a new difficulty.");
+
+                int newId = freeBeatmapIds.First();
+                freeBeatmapIds.Remove(newId);
                 allocatedBeatmapIds.Remove(newId);
                 playableBeatmap.BeatmapInfo.OnlineID = newId;
             }
